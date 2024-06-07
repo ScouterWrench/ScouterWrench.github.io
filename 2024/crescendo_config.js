07a576370e8d226fc48a4ba28d9f614e23888fba
ScouterWrench <allen.howard@hopkinsrobotics.org>
2024-06-06 20:29:26 -0500

    Changed Crescendo to Something else

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -2,7 +2,7 @@ var config_data = `
 {
   "dataFormat": "tsv",
   "title": "Scouting PASS 2024",
-  "page_title": "Crescendo",
+  "page_title": "Something else",
   "checkboxAs": "10",
   "prematch": [
     { "name": "Scouter Initials",
